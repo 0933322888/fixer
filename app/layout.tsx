b0a3c522d71a3e564eae9562a77534fa0314b90d
Andrii Vasylenko <0933322888@ukr.net>
2026-04-12 11:08:30 -0400

    added Google tags script

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-6MT281PXLJ"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6MT281PXLJ');
+`,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <Header />
         <Navbar />
